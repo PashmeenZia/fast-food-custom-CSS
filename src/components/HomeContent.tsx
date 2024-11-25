d37e332a27e6b520f7b169c70f9555bdc0dc7d89
PashmeenZia <pashmeenzia@gmail.com>
2024-11-25 20:01:41 +0500

    correction of the error

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -6,7 +6,7 @@ const HomeContent = () => {
         <section className='hero' style={{backgroundImage: "url(bg-image.jpg)"}}>
         <div>
             <h1 className='fade-in'>Welcome to Fast Food</h1>
-            <p>"Delicious Pizzas, Delivered Fresh to Your Doorstep!"</p>
+            <p>Delicious Pizzas, Delivered Fresh to Your Doorstep!</p>
             <button>Shop Now</button>
             </div>
         </section>
